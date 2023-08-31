cd07a3179d920930918ae8f4faa3d28ed1609bd5
Sezhz <sezhzqwe@gmail.com>
2023-08-31 23:50:45 +0300

    reviews-fixes-stylization

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import reviewsData from "./reviewsData";
 
 const Reviews = () => {
   return (
@@ -7,234 +8,43 @@ const Reviews = () => {
         <div className="reviews-header">
           <div className="reviews-title">
             <span>Наші клієнти</span>
-            <h2>Вiдгуки</h2>
+            <h2>Вiдгуки про компанiю та подорожi</h2>
           </div>
         </div>
         <div class="carousel-container">
           <div class="carousel-track">
-            <div class="carousel-item">
-              <div className="rating">
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
+            {reviewsData.map((review, index) => (
+              <div className="carousel-item" key={index}>
+                <div className="rating">
+                  {[...Array(review.rating)].map((_, i) => (
+                    <i className="fa-solid fa-star" key={i}></i>
+                  ))}
+                </div>
+                <div className="message">
+                  <p>{review.message}</p>
+                </div>
+                <div className="client-info">
+                  <h5>{review.name}</h5>
+                  <p>{review.location}</p>
+                </div>
               </div>
-              <div className="message">
-                <p>
-                  "Відпочинок на Мальдівах був просто неймовірним!
-                  Насолоджувалися безкрайніми білосніжними пляжами і кришталево
-                  чистою водою. Наш готель був райським притулком з водними
-                  віллами і дивовижними видами на океан. Обслуговування було на
-                  найвищому рівні, а кухня - справжнім гастрономічним
-                  задоволенням. Будемо пам'ятати цей відпочинок все життя!"
-                </p>
+            ))}
+            {reviewsData.map((review, index) => (
+              <div className="carousel-item" key={index}>
+                <div className="rating">
+                  {[...Array(review.rating)].map((_, i) => (
+                    <i className="fa-solid fa-star" key={i}></i>
+                  ))}
+                </div>
+                <div className="message">
+                  <p>{review.message}</p>
+                </div>
+                <div className="client-info">
+                  <h5>{review.name}</h5>
+                  <p>{review.location}</p>
+                </div>
               </div>
-              <div className="client-info">
-                <h5>Марi</h5>
-                <p>Мальдіви</p>
-              </div>
-            </div>
-            <div class="carousel-item">
-              <div className="rating">
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-              </div>
-              <div className="message">
-                <p>
-                  "Відпочинок в Єгипті був класним, але не без певних недоліків.
-                  Пляжі були гарними, і море тепле, але готель потребує
-                  реновації. Персонал був дружелюбним, але іноді недостатньо
-                  уважним."
-                </p>
-              </div>
-              <div className="client-info">
-                <h5>Олександр</h5>
-                <p>Єгипет</p>
-              </div>
-            </div>
-            <div class="carousel-item">
-              <div className="rating">
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-              </div>
-              <div className="message">
-                <p>
-                  "Відмінний сімейний відпочинок в Туреччині! Наш готель був
-                  затишним і охайним, з великими басейнами для дітей. Їжа була
-                  різноманітною, а вечірні розваги - веселими. Міські ринки
-                  виявилися скарбницею для шопоголіків. Усі члени сім'ї отримали
-                  незабутні враження!"
-                </p>
-              </div>
-              <div className="client-info">
-                <h5>Стефан</h5>
-                <p>Турцiя</p>
-              </div>
-            </div>
-            <div class="carousel-item">
-              <div className="rating">
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-              </div>
-              <div className="message">
-                <p>
-                  "Подорож до прекрасної префектури Кіото в Японії була
-                  надзвичайною! Наш готель, розташований в спокійному сільському
-                  регіоні, вражав своєю аутентичністю та затишком. Сніданки були
-                  надзвичайно смачні та аутентичні, а ввічливий персонал завжди
-                  готовий допомогти. Екскурсії до традиційних японських храмів
-                  та садів Кіото захоплювали подих. Ця подорож в Японію
-                  надихнула нас на відкриття багатств і культури цієї
-                  захоплюючої провінції."
-                </p>
-              </div>
-              <div className="client-info">
-                <h5>Даня</h5>
-                <p>Японiя</p>
-              </div>
-            </div>
-            <div class="carousel-item">
-              <div className="rating">
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-              </div>
-              <div className="message">
-                <p>
-                  "Відпустка на Балі була просто чарівною. Наша вілла з видом на
-                  рисові тераси була дивовижною. Смачна індонезійська кухня
-                  завоювала наші серця. Пляжі були чистими, а сноркелінг в
-                  коралових рифах - фантастичним. Ми 100% повернемося!"
-                </p>
-              </div>
-              <div className="client-info">
-                <h5>Євгенія</h5>
-                <p>Індонезія</p>
-              </div>
-            </div>
-
-            <div class="carousel-item">
-              <div className="rating">
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-              </div>
-              <div className="message">
-                <p>
-                  "Відпочинок на Мальдівах був просто неймовірним!
-                  Насолоджувалися безкрайніми білосніжними пляжами і кришталево
-                  чистою водою. Наш готель був райським притулком з водними
-                  віллами і дивовижними видами на океан. Обслуговування було на
-                  найвищому рівні, а кухня - справжнім гастрономічним
-                  задоволенням. Будемо пам'ятати цей відпочинок все життя!"
-                </p>
-              </div>
-              <div className="client-info">
-                <h5>Марi</h5>
-                <p>Мальдіви</p>
-              </div>
-            </div>
-            <div class="carousel-item">
-              <div className="rating">
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-              </div>
-              <div className="message">
-                <p>
-                  "Відпочинок в Єгипті був класним, але не без певних недоліків.
-                  Пляжі були гарними, і море тепле, але готель потребує
-                  реновації. Персонал був дружелюбним, але іноді недостатньо
-                  уважним."
-                </p>
-              </div>
-              <div className="client-info">
-                <h5>Олександр</h5>
-                <p>Єгипет</p>
-              </div>
-            </div>
-            <div class="carousel-item">
-              <div className="rating">
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-              </div>
-              <div className="message">
-                <p>
-                  "Відмінний сімейний відпочинок в Туреччині! Наш готель був
-                  затишним і охайним, з великими басейнами для дітей. Їжа була
-                  різноманітною, а вечірні розваги - веселими. Міські ринки
-                  виявилися скарбницею для шопоголіків. Усі члени сім'ї отримали
-                  незабутні враження!"
-                </p>
-              </div>
-              <div className="client-info">
-                <h5>Стефан</h5>
-                <p>Турцiя</p>
-              </div>
-            </div>
-            <div class="carousel-item">
-              <div className="rating">
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-              </div>
-              <div className="message">
-                <p>
-                  "Подорож до прекрасної префектури Кіото в Японії була
-                  надзвичайною! Наш готель, розташований в спокійному сільському
-                  регіоні, вражав своєю аутентичністю та затишком. Сніданки були
-                  надзвичайно смачні та аутентичні, а ввічливий персонал завжди
-                  готовий допомогти. Екскурсії до традиційних японських храмів
-                  та садів Кіото захоплювали подих. Ця подорож в Японію
-                  надихнула нас на відкриття багатств і культури цієї
-                  захоплюючої провінції."
-                </p>
-              </div>
-              <div className="client-info">
-                <h5>Даня</h5>
-                <p>Японiя</p>
-              </div>
-            </div>
-            <div class="carousel-item">
-              <div className="rating">
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-              </div>
-              <div className="message">
-                <p>
-                  "Відпустка на Балі була просто чарівною. Наша вілла з видом на
-                  рисові тераси була дивовижною. Смачна індонезійська кухня
-                  завоювала наші серця. Пляжі були чистими, а сноркелінг в
-                  коралових рифах - фантастичним. Ми 100% повернемося!"
-                </p>
-              </div>
-              <div className="client-info">
-                <h5>Євгенія</h5>
-                <p>Індонезія</p>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
